37e44502a8d1e40cb3f71aa76d7f9f516d6c3071
Diego Ruan <diegoruan614@gmail.com>
2025-08-09 23:19:20 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,22 +1,14 @@
-"use client";
-
 import { Github, Linkedin } from 'lucide-react';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 
 export function AppFooter() {
-  const [currentYear, setCurrentYear] = useState<number | null>(null);
-
-  useEffect(() => {
-    setCurrentYear(new Date().getFullYear());
-  }, []);
-
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="border-t py-6 md:py-8">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          © {currentYear || new Date().getFullYear()} Diego Ruan. Todos os direitos reservados.
+          © {currentYear} Diego Ruan. Todos os direitos reservados.
         </p>
         <div className="flex items-center gap-4">
           <Link href="#" target="_blank" rel="noreferrer" aria-label="Github">
