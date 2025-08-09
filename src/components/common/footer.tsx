2d6441cc88e71f8d0017c65a1bd6efb2b83c3aac
Diego Ruan <diegoruan614@gmail.com>
2025-08-09 23:15:42 +0000

    resolva esse erro uma vez por todas

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -5,17 +5,17 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
 export function AppFooter() {
-  const [isClient, setIsClient] = useState(false);
+  const [year, setYear] = useState(new Date().getFullYear());
 
   useEffect(() => {
-    setIsClient(true);
+    setYear(new Date().getFullYear());
   }, []);
 
   return (
     <footer className="border-t py-6 md:py-8">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          © {isClient ? new Date().getFullYear() : ''} Diego Ruan. Todos os direitos reservados.
+          © {year} Diego Ruan. Todos os direitos reservados.
         </p>
         <div className="flex items-center gap-4">
           <Link href="#" target="_blank" rel="noreferrer" aria-label="Github">
