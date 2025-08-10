b4eee42e186ce4ce7c397efcc78309d3428e3ad7
Diego Ruan <diegoruan614@gmail.com>
2025-08-10 17:18:21 +0000

    qUERO VERDE

--- src/components/common/letter-glitch.tsx
+++ src/components/common/letter-glitch.tsx
@@ -3,7 +3,7 @@
 import { useRef, useEffect } from "react";
 
 const LetterGlitch = ({
-  glitchColors = ["#ffffff", "#f0f0f0", "#e0e0e0"],
+  glitchColors = ["#00ff00", "#00dd00", "#00aa00"],
   glitchSpeed = 50,
   centerVignette = false,
   outerVignette = true,
